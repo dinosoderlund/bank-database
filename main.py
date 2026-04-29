cbb7e5ed715f6c778fcc2c3a7287520ea313f461
Dino Söderlund <152445219+dinosoderlund@users.noreply.github.com>
2026-04-29 21:43:35 +0200

    feat: added anomaly and suspicious logins to the fastapi (#7)

--- main.py
+++ main.py
@@ -31,3 +31,34 @@ def get_transactions():
     rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
     conn.close()
     return rows 
+
+@app.get("/suspicious")
+def get_suspicious():
+    #fetch ip with 5+ login failures within 5 min 
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT IPAddress, CAST(EventTime as DATETIME) AS EventTime, fails_5min 
+    FROM dbo.v_FailureBursts
+    ORDER BY fails_5min DESC
+    """)
+      #extraction of column names from cursor metadata 
+    columns = [col[0] for col in cursor.description]
+    #Zip column names with row values to create a list fo dicts 
+    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    return rows
+
+@app.get("/anomalies")
+def get_anomalies():
+    #fetch to long or to short sessions
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT LoginID, CAST(LoginTime as DATETIME) AS LoginTime,
+    CAST(LogoutTime as DATETIME) AS LogoutTime,
+    session_seconds, anomaly_type
+    FROM dbo.v_SessionAnomalies
+    """)
+    #extraction of column names from cursor metadata 
+    columns = [col[0] for col in cursor.description]
+    #Zip column names with row values to create a list fo dicts 
+    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    return rows
